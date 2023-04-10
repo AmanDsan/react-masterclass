4882a94a0e145685fcb4d765578a809638c76261
unknown <dubeyaman063@gmail.com>
2023-04-11 00:03:42 +0530

    assign1-masterclass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+
+export default function App() {
+  let random = Math.floor((Math.random()*99)+1)
+const [num, setNum] = useState(0)
+const multiplier = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+function handleClick(){
+  setNum(random)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Generate Number</button>
+     <h1>Random Number: {num}</h1>
+     <h2>Table</h2>
+     <div>
+     <table>
+                {multiplier.map((ele)=>{
+          return(
+            <tr>
+              <td>{num}</td>
+              <td>X</td>
+              <td>{ele}</td>
+              <td>=</td>
+            <td>{ele*num}</td>
+            </tr>
+          )
+        })}
+      
+     </table>
+     </div>
     </div>
   );
 }
-
-export default App;
